79fe97304cd2c0f7b6c794a257f6a2a025556309
kelvin.martins <kelvin.martins@sempreceub.com>
2024-08-30 21:56:13 -0300

    Adição dos exercícios 01 e 03

---
+++ Aula-03/Exercicio-01.py
@@ -0,0 +1,15 @@
+import Ferramentas.Inputs as fi
+
+nota1 = fi.input_int('Digite a primeira nota', 0,10)
+nota2 = fi.input_int('Digite a segunda nota', 0,10)
+nota3 = fi.input_int('Digite a terceira nota', 0,10)
+
+media = ((nota1 + nota2 + nota3) / 3)
+
+mencao = ''
+if media == 0 and media < 5:
+    mencao = 'Reprovado'
+elif media >= 5:
+    mencao = 'aprovado'
+
+print(mencao, f'com média: {media}')
---
+++ Aula-03/Exercicio-03.py
@@ -0,0 +1,11 @@
+#Exercício 03
+
+#Questão A
+for num in range(1, 101):
+    print(num)
+#Questão B
+for numero in range(100, 0 ,-1):
+    print(numero)
+#Questão C
+for numero2 in range(0,101, 10):
+    print(numero2)
