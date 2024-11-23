1782a56d7665a3bd1ee0ac192af8f8e2ce50bc24
kelvin.martins <kelvin.martins@sempreceub.com>
2024-11-22 21:23:59 -0300

    Ajustes em converter.py

--- util/converter.py
+++ util/converter.py
@@ -1,4 +1,30 @@
-from datetime import datetime
+import datetime
 
-def mysql_to_bra(dta):
-   return datetime.strftime(dta, '%d/%m/%Y')
+
+def mysql_to_bra(data_mysql):
+    """Converte uma data no formato MySQL para dd/mm/aaaa.
+    Args:
+      data_mysql: String representando a data no formato MySQL (YYYY-MM-DD).
+    Returns:
+      String com a data no formato dd/mm/aaaa.
+    """
+    if data_mysql == None:
+        return ""
+    else:
+        data_mysql = str(data_mysql)
+        return data_mysql[8:10] + "/" + data_mysql[5:7] + "/" + data_mysql[0:4]
+
+
+def bra_to_mysql(data_bra):
+    """Converte uma data no formato brasileiro (dd/mm/aaaa) para o formato MySQL (YYYY-MM-DD).
+    Args:
+      data_bra: String representando a data no formato brasileiro (dd/mm/aaaa).
+    Returns:
+      String com a data no formato MySQL (YYYY-MM-DD).
+    """
+
+    # Primeiro, convertemos a string para um objeto datetime
+    dt_mysql = data_bra[6:10] + "-" + data_bra[3:5] + "-" + data_bra[0:2]
+
+    # Em seguida, formatamos o objeto datetime para o formato MySQL
+    return dt_mysql
